10c6fe3a633c4c1c58d5edb43848d87b0ca81fe9
George Walter Colgrove IV <georgecolgrove@outlook.com>
2019-09-10 00:59:47 -0400

    Progress

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -460,8 +460,10 @@ public class YaVersion {
   // - SPEECHRECOGNIZER_COMPONENT_VERSION was incremented to 2
   // For YOUNG_ANDROID_VERSION 186:
   // - BLOCKS_LANGUAGE_VERSION was incremented to 27
+  // For YOUNG_ANDROID_VERSION 187:
+  // - Added pressure sensor
 
-  public static final int YOUNG_ANDROID_VERSION = 186;
+  public static final int YOUNG_ANDROID_VERSION = 187;
 
   // ............................... Blocks Language Version Number ...............................
 
@@ -1229,6 +1231,9 @@ public class YaVersion {
 
   //For PROXIMITYSENSOR_COMPONENT_VERSION: Initial Version
   public static final int PROXIMITYSENSOR_COMPONENT_VERSION = 1;
+
+  //For BAROMETER_COMPONENT_VERSION: Initial Version
+  public static final int BAROMETER_COMPONENT_VERION = 1;
 
   // Rendezvous Server Location
 
---
+++ appinventor/components/src/com/google/appinventor/components/runtime/BarometerSensor.java
@@ -0,0 +1,162 @@
+
+package com.google.appinventor.components.runtime;
+
+import com.google.appinventor.components.annotations.DesignerComponent;
+import com.google.appinventor.components.annotations.DesignerProperty;
+import com.google.appinventor.components.annotations.PropertyCategory;
+import com.google.appinventor.components.annotations.SimpleEvent;
+import com.google.appinventor.components.annotations.SimpleObject;
+import com.google.appinventor.components.annotations.SimpleProperty;
+import com.google.appinventor.components.common.ComponentCategory;
+import com.google.appinventor.components.common.PropertyTypeConstants;
+import com.google.appinventor.components.common.YaVersion;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+import java.util.List;
+
+@DesignerComponent(version = YaVersion.BAROMETER_COMPONENT_VERION,
+        description = "Non-visible component that can detect shaking and " +
+                "measure barometer ",
+        category = ComponentCategory.SENSORS,
+        nonVisible = true,
+        iconName = "images/accelerometersensor.png")
+@SimpleObject
+public class BarometerSensor extends AndroidNonvisibleComponent
+        implements OnStopListener, OnResumeListener, SensorComponent, SensorEventListener, Deleteable {
+
+    private final static String LOG_TAG = "BarometerSensor";
+    private final static boolean DEBUG = true;
+
+    // Indicates whether the barometer should generate events
+    private boolean enabled;
+    private final SensorManager sensorManager;
+    private Sensor barometerSensor;
+    private float currentMbar = 0f;
+
+    public BarometerSensor(ComponentContainer container) {
+        super(container.$form());
+
+        sensorManager = (SensorManager) container.$context().getSystemService(Context.SENSOR_SERVICE);
+        barometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        form.registerForOnResume(this);
+        form.registerForOnStop(this);
+        Enabled(true);
+        Log.d(LOG_TAG, "barometer created");
+    }
+
+    @Override
+    public void onDelete() {
+        if (enabled) {
+            stopListening();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        if (enabled) {
+            startListening();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (enabled) {
+            stopListening();
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (enabled) {
+            final float[] values = sensorEvent.values.clone();
+            BarometerChanged(values[0]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        // TODO(markf): Figure out if we actually need to do something here.
+    }
+
+
+    // Assumes that sensorManager has been initialized, which happens in constructor
+    private void startListening() {
+        sensorManager.registerListener(this, barometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    /**
+     * Specifies whether the sensor should generate events.  If true,
+     * the sensor will generate events.  Otherwise, no events are
+     * generated even if the device is accelerated or shaken.
+     *
+     * @param enabled  {@code true} enables sensor event generation,
+     *                 {@code false} disables it
+     */
+    @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_BOOLEAN,
+            defaultValue = "True")
+    @SimpleProperty
+    public void Enabled(boolean enabled) {
+        if (this.enabled == enabled) {
+            return;
+        }
+        this.enabled = enabled;
+        if (enabled) {
+            startListening();
+        } else {
+            stopListening();
+        }
+    }
+
+    // Assumes that sensorManager has been initialized, which happens in constructor
+    private void stopListening() {
+        sensorManager.unregisterListener(this);
+        currentMbar = 0f;
+    }
+
+    /**
+     * Available property getter method (read-only property).
+     *
+     * @return {@code true} indicates that an accelerometer sensor is available,
+     *         {@code false} that it isn't
+     */
+    @SimpleProperty(
+            category = PropertyCategory.BEHAVIOR)
+    public boolean Available() {
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_PRESSURE);
+        return (sensors.size() > 0);
+    }
+
+    /**
+     * If true, the sensor will generate events.  Otherwise, no events
+     * are generated even if the device is accelerated or shaken.
+     *
+     * @return {@code true} indicates that the sensor generates events,
+     *         {@code false} that it doesn't
+     */
+    @SimpleProperty(
+            category = PropertyCategory.BEHAVIOR)
+    public boolean Enabled() {
+        return this.enabled;
+    }
+
+    @SimpleProperty(
+            category = PropertyCategory.BEHAVIOR)
+    public float mbar() {
+        return this.currentMbar;
+    }
+
+    /**
+     * Indicates the barometer changed.
+     */
+    @SimpleEvent
+    public void BarometerChanged(float mbar) {
+        this.currentMbar = mbar;
+        EventDispatcher.dispatchEvent(this, "BarometerChanged", this.currentMbar);
+    }
+}
