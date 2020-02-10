4ce37db871cc312092593f812eae3b8d3925f976
George Walter Colgrove IV <georgecolgrove@outlook.com>
2020-02-09 21:00:09 -0500

    progress

--- appinventor/components/src/com/rit/appinventor/components/runtime/ros_bridge.java
+++ appinventor/components/src/com/rit/appinventor/components/runtime/ros_bridge.java
@@ -2,16 +2,20 @@ package com.rit.appinventor.components.runtime;
 
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import com.google.appinventor.components.annotations.DesignerComponent;
-import com.google.appinventor.components.annotations.DesignerProperty;
-import com.google.appinventor.components.annotations.SimpleFunction;
-import com.google.appinventor.components.annotations.SimpleObject;
+import com.google.appinventor.components.annotations.*;
 import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
 import com.google.appinventor.components.runtime.Deleteable;
 import com.google.appinventor.components.runtime.OnResumeListener;
 import com.google.appinventor.components.runtime.OnStopListener;
 import com.google.appinventor.components.runtime.ComponentContainer;
+import org.ros.address.InetAddressFactory;
+import org.ros.node.DefaultNodeMainExecutor;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMainExecutor;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
 @DesignerComponent(version = 1,
@@ -19,9 +23,15 @@ import com.google.appinventor.components.runtime.ComponentContainer;
         category = ComponentCategory.EXTENSION,
         nonVisible = true,
         iconName = "images/extension.png")
+@UsesPermissions(permissionNames = "android.permission.INTERNET")
+@UsesLibraries(libraries =
+        "apache_xmlrpc_client-0.3.6.jar,apache_xmlrpc_common-0.3.6.jar,apache_xmlrpc_server-0.3.6.jar," +
+        "commons-codec-1.3.jar,commons-httpclient-3.1.jar,commons-logging-1.1.1.jar,commons-pool-1.6.jar," +
+        "guava-12.0.1.jar,message_generation-0.3.3.jar,netty-3.5.2.jar,rosgraph_msgs-1.11.2.jar," +
+        "rosjava-0.3.6.jar,std_msgs-0.5.10.jar,ws-commons-util-1.0.2.jar")
 @SimpleObject(external = true)
 public class ros_bridge extends AndroidNonvisibleComponent implements OnStopListener, OnResumeListener, Deleteable {
-
+    NodeConfiguration configuration;
     public ros_bridge(ComponentContainer container) {
         super(container.$form());
     }
@@ -56,8 +66,10 @@ public class ros_bridge extends AndroidNonvisibleComponent implements OnStopList
      * URI provided as a string parameter
      */
     @DesignerProperty
-    public void connectToMaster(String URI) {
-        //TODO
+    public void connectToMaster(String uri) throws URISyntaxException {
+        java.net.URI masterUri = new URI(uri);
+
+        configuration = NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress(), masterUri);
     }
 
     /**
