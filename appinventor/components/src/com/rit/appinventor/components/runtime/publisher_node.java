3e2a3612015b7ea21923159f65c4f955e0aed220
Ryan Connors <connorsryanj@gmail.com>
2020-02-17 17:58:16 -0500

    Added skeleton code for publisher nodes

---
+++ appinventor/components/src/com/rit/appinventor/components/runtime/publisher_node.java
@@ -0,0 +1,32 @@
+package com.rit.appinventor.components.runtime;
+
+import org.ros.concurrent.CancellableLoop;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+import org.ros.node.Node;
+import org.ros.node.NodeMain;
+import org.ros.node.topic.Publisher;
+
+public class publisher_node extends AbstractNodeMain implements NodeMain {
+
+    @Override
+    public void onStart(ConnectedNode connectedNode) {
+        //TODO
+    }
+
+    @Override
+    public void onShutdown(Node node) {
+        //TODO
+    }
+
+    @Override
+    public void onShutdownComplete(Node node) {
+        //TODO
+    }
+
+    @Override
+    public void onError(Node node, Throwable throwable) {
+        //TODO
+    }
+}
